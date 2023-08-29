2ff6f3176e34d4161a6c04e31b5ec224f5e016c5
Parth <82119640+malaviya-parth@users.noreply.github.com>
2023-08-29 21:45:18 +0530

    Added constructor with params

--- SpaceCraft.js
+++ SpaceCraft.js
@@ -1,5 +1,10 @@
 class SpaceCraft {
-
+    constructor(x, y, z, direction) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.direction = direction;
+    }
 }
 
 module.exports = SpaceCraft;
